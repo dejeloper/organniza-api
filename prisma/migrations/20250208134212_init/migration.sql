928262121e084bcbdc4027e8ba50d1cf36f24f0b
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-08 08:44:19 -0500

    refactor: Update product model

--- prisma/migrations/20250208134212_init/migration.sql
+++ prisma/migrations/20250208134212_init/migration.sql
@@ -3,13 +3,9 @@ CREATE TABLE "Product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "price" REAL NOT NULL,
-    "category" TEXT,
-    "clasification" TEXT,
+    "category" TEXT NOT NULL,
+    "clasification" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "deletedAt" DATETIME
+    "updatedAt" DATETIME NOT NULL
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
