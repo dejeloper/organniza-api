2444c103b30e4241fac1b2431b80c4119f232037
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-09 01:09:04 -0500

    refactor: Update CreateProductDto & UpdateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,17 +1,3 @@
-// import {Transform} from 'class-transformer';
-// import {IsString, IsNumber} from 'class-validator';
-
 import {Product} from "@prisma/client";
 
-// export class CreateProductDto {
-// 	@IsString()
-// 	readonly name: string;
-// 	@IsNumber()
-// 	readonly price: number;
-// 	@IsString()
-// 	readonly category: string;
-// 	@IsString()
-// 	readonly clasification: string; 
-// }
-
-export type CreateProductDto = Omit<Product, 'id' | 'enabled' | 'createdAt' | 'updatedAt' | 'deletedAt'>;
+export type CreateProductDto = Omit<Product, 'id' | 'enabled' | 'createdAt' | 'updatedAt'>;
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,3 @@
 import {CreateProductDto} from "./create-product.dto";
 
-//export class UpdateProductDto extends PartialType(CreateProductDto) { };
 export type UpdateProductDto = Partial<CreateProductDto>;
